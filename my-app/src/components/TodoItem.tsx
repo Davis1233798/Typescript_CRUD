6f67ae2579f79cd3731ed254444d07e2749e69a5
Davis1233798 <davis1233798@gmail.com>
2022-10-18 12:34:53 +0800

    final

--- my-app/src/components/TodoItem.tsx
+++ my-app/src/components/TodoItem.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React from "react"
 
-const TodoItem = () => {
+type Props = TodoProps & {
+  updateTodo: (todo: ITodo) => void
+  deleteTodo: (_id: string) => void
+}
+
+const Todo: React.FC<Props> = ({ todo, updateTodo, deleteTodo }) => {
+  const checkTodo: string = todo.status ? `line-through` : ""
   return (
-    <div>TodoItem</div>
+    <div className="Card">
+      <div className="Card--text">
+        <h1 className={checkTodo}>{todo.name}</h1>
+        <span className={checkTodo}>{todo.description}</span>
+      </div>
+      <div className="Card--button">
+        <button
+          onClick={() => updateTodo(todo)}
+          className={todo.status ? `hide-button` : "Card--button__done"}
+        >
+          Complete
+        </button>
+        <button
+          onClick={() => deleteTodo(todo._id)}
+          className="Card--button__delete"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
   )
 }
 
-export default TodoItem
-// type Props = TodoProps & {
-//     updateTodo:(todo:ITodo)=>void
-//     deleteTodo:(_id:string)=>void
-// }
-
-// //const Todo:React.FC<Props> = ({todo,updateTodo,deleteTodo})
+export default Todo
